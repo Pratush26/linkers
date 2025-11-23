053a85a979a3a7be54427ed5d7faf06926a5bcff
Pratush <pratushbarua26@gmail.com>
2025-11-23 13:31:30 +0600

    fixed proxy file

---
+++ proxy.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { auth } from './auth'
+
+export async function proxy(request: NextRequest) {
+    const session = await auth()
+    if (!session) return NextResponse.redirect(new URL('/home', request.url))
+}
+
+export const config = {
+    matcher: '/dashboard/:path*',
+}
